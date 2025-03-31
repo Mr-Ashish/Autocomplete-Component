aa8414b9bd2e5cfe09f71f93f8322f7e3cdadd1d
Mr-Ashish <32571204+Mr-Ashish@users.noreply.github.com>
2025-03-31 09:48:59 +0530

    feat(eslint,jest): add basic eslint and jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.(ts|tsx|js|jsx)$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  transformIgnorePatterns: ["node_modules", "\\.css$"],
+  moduleNameMapper: {
+    "\\.(css|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/test/setup.ts"],
+};
+
+export default config;
